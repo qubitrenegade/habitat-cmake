6f3715e1be8ffa52fb0503986dab8ec2209ada2d
qubitrenegade <qubitrenegade@gmail.com>
2018-07-14 00:00:54 -0600

    The commit that started it all...

---
+++ plan.sh
@@ -0,0 +1,37 @@
+pkg_name=cmake
+pkg_origin=qubitrenegade
+_base_version=3.10
+pkg_version=${_base_version}.2
+pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_license=('BSD-3-Clause')
+pkg_description="CMake is an open-source, cross-platform family of tools designed to build, test and package software"
+pkg_upstream_url="https://cmake.org/"
+pkg_source="https://cmake.org/files/v${_base_version}/cmake-${pkg_version}.tar.gz"
+pkg_shasum=80d0faad4ab56de07aa21a7fc692c88c4ce6156d42b0579c6962004a70a3218b
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/make
+  core/gcc
+  core/curl
+  core/zlib
+  core/bzip2
+)
+
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_bin_dirs=(bin)
+
+do_build() {
+  ./bootstrap --system-curl
+  ./configure --prefix="${pkg_prefix}"
+  make -j "$(nproc)"
+}
+
+do_check() {
+  make test
+}
